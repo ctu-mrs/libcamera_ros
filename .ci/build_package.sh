2ffd2b04fb1735417c8c07ca48472a512f554384
Vojtech Spurny <spurny@fly4future.com>
2024-03-05 15:45:56 +0100

    install properly header files in .ci/build_package.sh

--- .ci/build_package.sh
+++ .ci/build_package.sh
@@ -78,7 +78,7 @@ TMP_PATH=/tmp/libcamera
 mkdir -p $TMP_PATH/package/DEBIAN
 mkdir -p $TMP_PATH/package/opt/ros/noetic/share
 
-cp -r $WORKSPACE_PATH/install/include/libcamera/libcamera $TMP_PATH/package/opt/ros/noetic/include
+cp -r $WORKSPACE_PATH/install/include/libcamera $TMP_PATH/package/opt/ros/noetic/include
 cp -r $WORKSPACE_PATH/install/share/libcamera $TMP_PATH/package/opt/ros/noetic/share
 cp -r $WORKSPACE_PATH/install/libexec $TMP_PATH/package/opt/ros/noetic/libexec
 cp -r $WORKSPACE_PATH/install/lib $TMP_PATH/package/opt/ros/noetic/lib
